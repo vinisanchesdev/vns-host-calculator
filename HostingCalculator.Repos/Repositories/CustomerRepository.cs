96695c1dd18f2ce002e47e50a26e7041581b3e35
vinisanchesdev <viniciussanches22@outlook.com>
2018-10-08 20:11:07 -0300

    Add project files.

---
+++ HostingCalculator.Repos/Repositories/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity = HostingCalculator.Domain.Entities;
+
+namespace HostingCalculator.Repos.Repositories
+{
+    public class CustomerRepository
+    {
+        DatabaseContext db = new DatabaseContext();
+
+        public List<Entity.Customer> GetAllCustomers()
+        {
+            List<Entity.Customer> customers = new List<Entity.Customer>();
+
+            customers.Add(new Entity.Customer(Guid.NewGuid(), "Vini", "vinicius.guimaraes@rakuten.com.br", "22-06-1993"));
+            customers.Add(new Entity.Customer(Guid.NewGuid(), "Carla", "carla.jemaitis@gmail.com", "06-05-1994"));
+
+            return customers;
+        }
+
+        public void AddCustomer()
+        {
+            var customer = new Entity.Customer(Guid.NewGuid(), "Carla", "carla.jemaitis@gmail.com", "06-05-1994");
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+        }
+
+
+               
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# vns-hosting-calculator
+Calculate the best price for hosting in our beach house .
